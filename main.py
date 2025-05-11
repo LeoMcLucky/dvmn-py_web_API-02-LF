4041a8afc5d8ab639bf73bb745eb02675d4ece07
Lev <47698500+LeoMcLucky@users.noreply.github.com>
2025-05-12 02:00:39 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from urllib.parse import urlparse
 
 
 def is_shorten_link(token, url):
-    link_short = True
     url_vk = 'https://api.vk.ru/method/utils.getLinkStats'
     parsed_url = urlparse(url)
     key = parsed_url.path.lstrip('/')
@@ -17,10 +16,8 @@ def is_shorten_link(token, url):
     }
     response = requests.get(url_vk, params=payload)
     response.raise_for_status()
-    check_response = response.json()
-    if 'error' in check_response:
-        link_short = False
-    return link_short
+    link_stats = response.json()
+    return 'error' not in link_stats
 
 
 def shorten_link(token, url):
@@ -48,24 +45,23 @@ def count_clicks(token, short_url):
     }
     response = requests.get(url_vk, params=payload)
     response.raise_for_status()
-    clicks_counted = response.json()
-    return clicks_counted['response']['stats'][0]['views']
+    counted_clicks = response.json()
+    return counted_clicks['response']['stats'][0]['views']
 
 
 def main():
     load_dotenv()
-    token_api_vk = os.getenv('TOKEN_API_VK')
+    api_vk_token = os.getenv('API_VK_TOKEN')
     url = input("Введите ссылку: ")
-    check_url = is_shorten_link(token_api_vk, url)
-    if check_url:
+    if is_shorten_link(api_vk_token, url):
         try:
-            click_count = count_clicks(token_api_vk, url)
+            click_count = count_clicks(api_vk_token, url)
             print("Всего по ссылке было сделано кликов:", click_count)
         except KeyError:
             print("Ошибка, введите сокращенную ссылку корректно.")
     else:
         try:
-            short_url = shorten_link(token_api_vk, url)
+            short_url = shorten_link(api_vk_token, url)
             print("Сокращенная ссылка:", short_url)
         except KeyError:
             print("Ошибка, введите ссылку.")
